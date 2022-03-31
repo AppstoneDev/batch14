c33cfbc7624a3a97e453c3474c2c07e1807ad419
gautamakash91 <gautamakash91@gmail.com>
2022-03-31 20:01:29 +0530

    fetch and mapping

---
+++ src/components/fetch/fetcher.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react"
+
+export default function Fetcher() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    handleClick();
+  }, [])
+
+  function handleClick() {
+    fetch("https://reqres.in/api/users?page=1")
+      .then((response) => { return response.json() })
+      .then((resJson) => {
+        setUsers(resJson.data);
+      })
+  }
+
+  return (
+    <div>
+      {/* <button onClick={() => { handleClick() }}>get users</button> */}
+
+      {users.map((user, index) => {
+        return (<div>{user.first_name} {user.last_name}</div>)
+      }
+      )}
+    </div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 
-import ConditionalRendering from "./components/lifecycle/conditionalRendering";
+import Fetcher from "./components/fetch/fetcher";
 
 ReactDOM.render(
-    <ConditionalRendering />
+    <Fetcher />
   ,
   document.getElementById('root')
 );
