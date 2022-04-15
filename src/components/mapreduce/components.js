563966ff3aef3396a52f3f4b4a8bf8053ccfe914
gautamakash91 <gautamakash91@gmail.com>
2022-04-15 19:18:00 +0530

    mapreduce and calculator

--- src/components/mapreduce/components.js
+++ src/components/mapreduce/components.js
@@ -1,17 +1,17 @@
 export default function Sum(props) {
     return (
-        <div>{props.vala + props.valb}</div>
+        <div>sum is: {props.vala + props.valb}</div>
     )
 }
 
 export function Difference(props) {
     return (
-        <div>{props.vala - props.valb}</div>
+        <div>difference is: {props.vala - props.valb}</div>
     )
 }
 
 export function Product(props) {
     return (
-        <div>{props.vala * props.valb}</div>
+        <div>product is: {props.vala * props.valb}</div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -12,4 +12,5 @@ ReactDOM.render(
 
 // ASSIGNMENTS
 //1. Create 2 components. 1 should be default and 1 should not be. Render both on the screen at the same time.
+//x. pagination in fetch user API. user should be able to click on next or prev button to fetch users of that page. 
 reportWebVitals();
